8436e5f15f368ab3c9f647911529fc726eb19144
RektInator <mlmmlangeveld@gmail.com>
2020-01-12 16:53:06 +0100

    added NNG

---
+++ deps/premake/nng.lua
@@ -0,0 +1,51 @@
+nng = {
+	source = path.join(dependencies.basePath, "nng"),
+}
+
+function nng.import()
+	links {
+		"nng"
+	}
+
+	nng.includes()
+end
+
+function nng.includes()
+	includedirs {
+		path.join(nng.source, "include"),
+	}
+
+	defines {
+		"NNG_PLATFORM_WINDOWS"
+	}
+end
+
+function nng.project()
+	project "nng"
+		language "C"
+
+		nng.includes()
+
+		includedirs {
+			path.join(nng.source, "src"),
+		}
+
+		files {
+			path.join(nng.source, "src/**.c"),
+			path.join(nng.source, "src/**.h"),
+		}
+
+		removefiles {
+			path.join(nng.source, "src/platform/posix/**"),
+			path.join(nng.source, "src/**/*test.c"),
+			path.join(nng.source, "src/compat/**"),
+			path.join(nng.source, "src/transport/tls/**"),
+			path.join(nng.source, "src/transport/zerotier/**"),
+			path.join(nng.source, "src/supplemental/tls/**"),
+		}
+
+		warnings "Off"
+		kind "StaticLib"
+end
+
+table.insert(dependencies, nng)
