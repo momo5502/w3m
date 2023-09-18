e91b95f2d13d3d7a57eb2d585cf342667718d304
momo5502 <mauriceheumann@gmail.com>
2023-09-18 20:10:07 +0200

    Add missing includes

--- src/common/network/address.hpp
+++ src/common/network/address.hpp
@@ -8,6 +8,19 @@
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#else
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+
+#define ZeroMemory(x, y) memset(x, 0, y)
+
 #endif
 
 #include <string>
