ae21084cc44106a24121f29cb5042234cd5b0e3a
momo5502 <mauriceheumann@gmail.com>
2023-09-18 20:37:31 +0200

    IO fixes

--- src/common/utils/io.cpp
+++ src/common/utils/io.cpp
@@ -20,17 +20,16 @@ namespace utils::io
 #endif
 	}
 
-	bool file_exists(const std::string& file)
+	bool file_exists(const std::filesystem::path& file)
 	{
 		return std::ifstream(file).good();
 	}
 
-	bool write_file(const std::string& file, const std::string& data, const bool append)
+	bool write_file(const std::filesystem::path& file, const std::string& data, const bool append)
 	{
-		const auto pos = file.find_last_of("/\\");
-		if (pos != std::string::npos)
+		if (file.has_parent_path())
 		{
-			create_directory(file.substr(0, pos));
+			io::create_directory(file.parent_path());
 		}
 
 		std::ofstream stream(
@@ -46,97 +45,29 @@ namespace utils::io
 		return false;
 	}
 
-	std::string read_file(const std::string& file)
+	std::string read_file(const std::filesystem::path& file)
 	{
 		std::string data;
 		read_file(file, &data);
 		return data;
 	}
 
-	bool read_file(const std::string& file, std::string* data)
+	bool read_file(const std::filesystem::path& file, std::string* data)
 	{
 		if (!data) return false;
 		data->clear();
 
-		if (file_exists(file))
+		std::ifstream stream(file, std::ios::binary);
+		if (!stream) return false;
+
+		stream.seekg(0, std::ios::end);
+		const std::streamsize size = stream.tellg();
+		stream.seekg(0, std::ios::beg);
+
+		if (size > -1)
 		{
-			std::ifstream stream(file, std::ios::binary);
-			if (!stream.is_open()) return false;
-
-			stream.seekg(0, std::ios::end);
-			const std::streamsize size = stream.tellg();
-			stream.seekg(0, std::ios::beg);
-
-			if (size > -1)
-			{
-				data->resize(static_cast<std::uint32_t>(size));
-				stream.read(data->data(), size);
-				stream.close();
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	std::size_t file_size(const std::string& file)
-	{
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
-
-			if (stream.good())
-			{
-				stream.seekg(0, std::ios::end);
-				return static_cast<std::size_t>(stream.tellg());
-			}
-		}
-
-		return 0;
-	}
-
-	bool create_directory(const std::filesystem::path& directory)
-	{
-		return std::filesystem::create_directories(directory);
-	}
-
-	bool directory_exists(const std::filesystem::path& directory)
-	{
-		return std::filesystem::is_directory(directory);
-	}
-
-	bool directory_is_empty(const std::filesystem::path& directory)
-	{
-		return std::filesystem::is_empty(directory);
-	}
-
-	void copy_folder(const std::filesystem::path& src, const std::filesystem::path& target)
-	{
-		std::filesystem::copy(src, target,
-		                      std::filesystem::copy_options::overwrite_existing |
-		                      std::filesystem::copy_options::recursive);
-	}
-
-
-	bool file_exists(const std::wstring& file)
-	{
-		return std::ifstream(file).good();
-	}
-
-	bool write_file(const std::wstring& file, const std::string& data, const bool append)
-	{
-		const auto pos = file.find_last_of(L"/\\");
-		if (pos != std::string::npos)
-		{
-			create_directory(file.substr(0, pos));
-		}
-
-		std::ofstream stream(
-			file, std::ios::binary | std::ofstream::out | (append ? std::ofstream::app : 0));
-
-		if (stream.is_open())
-		{
-			stream.write(data.data(), static_cast<std::streamsize>(data.size()));
+			data->resize(static_cast<std::uint32_t>(size));
+			stream.read(data->data(), size);
 			stream.close();
 			return true;
 		}
@@ -144,53 +75,43 @@ namespace utils::io
 		return false;
 	}
 
-	std::string read_file(const std::wstring& file)
+	std::size_t file_size(const std::filesystem::path& file)
 	{
-		std::string data;
-		read_file(file, &data);
-		return data;
-	}
+		std::ifstream stream(file, std::ios::binary);
 
-	bool read_file(const std::wstring& file, std::string* data)
-	{
-		if (!data) return false;
-		data->clear();
-
-		if (file_exists(file))
+		if (stream)
 		{
-			std::ifstream stream(file, std::ios::binary);
-			if (!stream.is_open()) return false;
-
 			stream.seekg(0, std::ios::end);
-			const std::streamsize size = stream.tellg();
-			stream.seekg(0, std::ios::beg);
-
-			if (size > -1)
-			{
-				data->resize(static_cast<uint32_t>(size));
-				stream.read(data->data(), size);
-				stream.close();
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	std::size_t file_size(const std::wstring& file)
-	{
-		if (file_exists(file))
-		{
-			std::ifstream stream(file, std::ios::binary);
-
-			if (stream.good())
-			{
-				stream.seekg(0, std::ios::end);
-				return static_cast<std::size_t>(stream.tellg());
-			}
+			return static_cast<std::size_t>(stream.tellg());
 		}
 
 		return 0;
+	}
+
+	bool create_directory(const std::filesystem::path& directory)
+	{
+		std::error_code ec{};
+		return std::filesystem::create_directories(directory, ec) && !ec;
+	}
+
+	bool directory_exists(const std::filesystem::path& directory)
+	{
+		std::error_code ec{};
+		return std::filesystem::is_directory(directory, ec) && !ec;
+	}
+
+	bool directory_is_empty(const std::filesystem::path& directory)
+	{
+		std::error_code ec{};
+		return std::filesystem::is_empty(directory, ec) && !ec;
+	}
+
+	void copy_folder(const std::filesystem::path& src, const std::filesystem::path& target)
+	{
+		std::error_code ec{};
+		std::filesystem::copy(src, target,
+		                      std::filesystem::copy_options::overwrite_existing |
+		                      std::filesystem::copy_options::recursive, ec);
 	}
 
 	std::vector<std::filesystem::path> list_files(const std::filesystem::path& directory, const bool recursive)
