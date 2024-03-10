004024367c2cf9e7eb8e9eab91b858ad39328564
momo5502 <mauriceheumann@gmail.com>
2024-03-10 18:13:21 +0100

    Better launcher

--- src/client/module/launcher.cpp
+++ src/client/module/launcher.cpp
@@ -1,7 +1,8 @@
 #include "../std_include.hpp"
-
 #include "../loader/component_loader.hpp"
-#include "../loader/loader.hpp"
+#include "../resource.hpp"
+
+#include <utils/nt.hpp>
 
 #include <momo/html_ui.hpp>
 
@@ -21,76 +22,7 @@ namespace
 				window.close();
 			});
 
-			window.load_html(R"code(
-<!DOCTYPE html>
-<html>
-
-<head>
-    <style>
-        html {
-            height: 100%;
-            background: linear-gradient(135deg, #FFFFFF, #01CDFF);
-        }
-
-        * {
-            font-family: -apple-system, BlinkMacSystemFont, segoe ui, Roboto, Oxygen, Ubuntu, Cantarell, open sans, helvetica neue, sans-serif;
-        }
-
-        h1 {
-            color: rgb(57, 57, 57);
-        }
-
-        button {
-            margin: 5px;
-            display: inline-block;
-            outline: none;
-            cursor: pointer;
-            font-size: 14px;
-            line-height: 1;
-            border-radius: 10px;
-            transition-property: background-color, border-color, color, box-shadow, filter;
-            transition-duration: .3s;
-            border: 1px solid transparent;
-            letter-spacing: 2px;
-            min-width: 160px;
-            text-transform: uppercase;
-            white-space: normal;
-            font-weight: 700;
-            text-align: center;
-            padding: 16px 14px 18px;
-            color: rgb(57, 57, 57);
-            border: 2px solid rgb(57, 57, 57);
-            background-color: transparent;
-            height: 48px;
-        }
-
-        button:hover,
-        button.active {
-            color: #fff;
-            background-color: rgb(57, 57, 57);
-        }
-
-		button[disabled],
-		button[disabled]:hover {
-			cursor: default;
-			opacity: 0.5;
-			color: rgb(57, 57, 57);
-			background-color: transparent;
-		}
-    </style>
-</head>
-
-<body>
-    <center>
-        <h1>Witcher 3: Online</h1>
-        <br>
-        <button onclick="window.external.launch(11)">DirectX 11</button>
-		<button onclick="window.external.launch(12)" disabled>DirectX 12</button>
-    </center>
-</body>
-
-</html>
-)code");
+			window.load_html(utils::nt::load_resource(MAIN_MENU));
 
 			momo::html_ui::show_windows();
 
--- src/client/resource.hpp
+++ src/client/resource.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
+#define APP_ICON 102
 #define IMAGE_SPLASH 300
+#define MAIN_MENU    301
